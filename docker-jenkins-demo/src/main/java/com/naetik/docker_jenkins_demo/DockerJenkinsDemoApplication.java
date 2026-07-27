5dafe22417f85f57bba6265bb955ff212b838352
Naitikk <Naetiksoni.08@gmail.com>
2026-07-28 02:03:43 +0530

    Added Spring Boot Project

---
+++ docker-jenkins-demo/src/main/java/com/naetik/docker_jenkins_demo/DockerJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.naetik.docker_jenkins_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsDemoApplication.class, args);
+	}
+
+}
